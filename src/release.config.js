316d889bc76083c90058d39d805115d8c1026da8
Jools Arts <jools.arts@gmail.com>
2022-08-01 14:41:50 +0100

    Included a number of ZIP assets

--- src/release.config.js
+++ src/release.config.js
@@ -1,5 +1,14 @@
 module.exports = {
     branches: "master",
     repositoryUrl: "https://github.com/jools-arts/react-app",
-    plugins: ['@semantic-release/commit-analyzer', '@semantic-release/release-notes-generator', '@semantic-release/npm', '@semantic-release/github']
+    plugins: [
+        '@semantic-release/commit-analyzer', 
+        '@semantic-release/release-notes-generator', 
+        '@semantic-release/npm', 
+        ['@semantic-release/github', {
+            assets: [
+                {path: "build.zip", label: "Build"},
+                {path: "coverage.zip", label: "Coverage"}
+            ]
+        }]
 }
